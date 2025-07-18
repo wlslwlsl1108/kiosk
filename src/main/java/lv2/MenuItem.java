84605dcb95f28c99b1c6f1eeebfffa5ee259469a
jinhee kim <lsa68807@hanmail.net>
2025-07-18 15:50:18 +0900

    lv2 필드 선언 완료

--- src/main/java/lv2/MenuItem.java
+++ src/main/java/lv2/MenuItem.java
@@ -1,5 +1,13 @@
 package lv2;
 
+import java.util.List;
+import java.util.ArrayList;
+
 public class MenuItem {
+    String name;  // 이름 필드 선언
+    int price;   // 가격 필드 선언
+    String description; // 설명 필드 선언
+
+    private final List<MenuItem> menuItems = new ArrayList<>();
 
 }
