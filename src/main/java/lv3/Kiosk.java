fb16db48476d7fc5226437e450048beedd148ff4
jinhee kim <lsa68803@naver.com>
2025-07-22 18:04:35 +0900

    lv3 : 문자 입력 시 오류 메시지 후 재입력 가능

--- src/main/java/lv3/Kiosk.java
+++ src/main/java/lv3/Kiosk.java
@@ -2,6 +2,7 @@ package lv3;
 
 import java.util.List;
 import java.util.Scanner;
+import java.util.InputMismatchException;
 
 public class Kiosk {
 
@@ -31,43 +32,49 @@ public class Kiosk {
         // 숫자 입력 안내문 //
         System.out.print("메뉴를 번호로 선택하세요: ");
 
-        // 숫자 입력받기 //
-        int number = scanner.nextInt();
+        // 오류처리 - 문자
+            try {
+                // 숫자 입력받기 //
+                int number = scanner.nextInt();
 
+                // 오류처리 - 음수, 다른 숫자
+                // .size() 사용하면 메뉴 갯수 변경시 영향 x
+                if (number < 0 || number > menuItems.size()) {
+                    System.out.println("없는 메뉴입니다. 숫자를 다시 입력해주세요.");
+                    scanner.nextLine();  // 무한반복 안되도록 입력 버퍼 비워주기
+                    continue;
+                }
 
-            // 오류처리 - 음수, 다른 숫자
-            // .size() 사용하면 메뉴 갯수 변경시 영향 x
-            if (number < 0 || number > menuItems.size()) {
-                System.out.println("없는 메뉴입니다. 다시 입력해주세요.");
-                scanner.nextLine();  // 무한반복 안되도록 입력 버퍼 비워주기
-                continue;
+                // 입력된 숫자에 따른 처리 //
+                switch (number) {
+                    case 1:
+                        System.out.println("선택한 메뉴 : 1. ShackBurger   | w 6.9 | 토마토 양상추 쉑소스가 토핑된 치즈버거");
+                        break;
+
+                    case 2:
+                        System.out.println("선택한 메뉴 : 2. SmokeShack    | w 8.9 | 베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거");
+                        break;
+
+                    case 3:
+                        System.out.println("선택한 메뉴 : 3. CheeseBurger  | w 6.9 | 포테이토 번과 비프패티, 치즈가 토핑된 치즈버거");
+                        break;
+
+                    case 4:
+                        System.out.println("선택한 메뉴 : 4. HamBurger     | w 5.4 | 비프패티를 기반으로 야채가 들어간 기본버거");
+                        break;
+
+                    case 0:
+                        System.out.println("프로그램을 종료합니다.");
+                        System.exit(0);
+                        break;
+
+                }
             }
-
-
-        // 입력된 숫자에 따른 처리 //
-        switch (number) {
-            case 1:
-                System.out.println("선택한 메뉴 : 1. ShackBurger   | w 6.9 | 토마토 양상추 쉑소스가 토핑된 치즈버거");
-                break;
-
-            case 2:
-                System.out.println("선택한 메뉴 : 2. SmokeShack    | w 8.9 | 베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거");
-                break;
-
-            case 3:
-                System.out.println("선택한 메뉴 : 3. CheeseBurger  | w 6.9 | 포테이토 번과 비프패티, 치즈가 토핑된 치즈버거");
-                break;
-
-            case 4:
-                System.out.println("선택한 메뉴 : 4. HamBurger     | w 5.4 | 비프패티를 기반으로 야채가 들어간 기본버거");
-                break;
-
-            case 0:
-                System.out.println("프로그램을 종료합니다.");
-                System.exit(0);
-                break;
-
-        }
+            catch (InputMismatchException e) {
+                System.out.println("문자는 입력할 수 없습니다. 숫자를 다시 입력해주세요.");
+                scanner.nextLine();  // 입력 버퍼 비우기
+                continue; // 재입력 받기
+            }
         }
     }
 }
