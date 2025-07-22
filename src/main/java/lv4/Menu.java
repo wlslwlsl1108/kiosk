3105e182d674390b03cde4a0f269327d466c480f
jinhee kim <lsa68803@naver.com>
2025-07-22 22:00:12 +0900

    lv4: items(하위 항목) 필드 선언 및 초기화

--- src/main/java/lv4/Menu.java
+++ src/main/java/lv4/Menu.java
@@ -8,10 +8,20 @@ public class Menu {
     // 상위 카테고리 도입
 
     // 상위 카테고리 정의//
-    String berger;  // 이름 필드 선언
+    String categoryName;  // 필드 선언
 
     // 생성자 // - MenuItem 클래스의 객체 생성 시 호출되는 메서드
-    public Menu(String berger, String drink) {
-        this.berger = berger;
+    public Menu(String categoryName) {
+        this.categoryName = categoryName;
+        // items 초기화 -> 초기화안하면 NullPointerException 오류 발생가능성 있음
+        this.items = new ArrayList<>();
+    }
+
+    // 하위 항목 필드 선언
+    public List<MenuItem> items;
+
+    // 하위 항목 추가(연결)
+    public void addMenuItem(MenuItem item){
+        items.add(item);
     }
 }
