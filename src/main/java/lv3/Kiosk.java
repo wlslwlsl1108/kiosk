acfe7bc7f54ad12a00673f022edd51dff484ef7f
jinhee kim <lsa68803@naver.com>
2025-07-22 11:45:37 +0900

    lv3 MenuItem 관리하는 리스트(필드) 생성

---
+++ src/main/java/lv3/Kiosk.java
@@ -0,0 +1,11 @@
+package lv3;
+
+import java.util.List;
+
+public class Kiosk {
+
+    // MenuItem을 관리하는 리스트(필드) //
+    public List<MenuItem> menuItems;
+
+
+}
---
+++ src/main/java/lv3/MenuItem.java
@@ -0,0 +1,23 @@
+package lv3;
+
+import java.util.List;
+import java.util.ArrayList;
+
+
+    // 클래스 정의 //
+    public class MenuItem {
+
+        // 클래스의 필드(멤버 변수) 정의//
+        String name;  // 이름 필드 선언
+        Double price;   // 가격 필드 선언
+        String description; // 설명 필드 선언
+        int number;
+
+        // 생성자 // - MenuItem 클래스의 객체 생성 시 호출되는 메서드
+        public MenuItem(String name, double price, String description) {
+            this.name = name;
+            this.price = price;
+            this.description = description;
+        }
+    }
+
